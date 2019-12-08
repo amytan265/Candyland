88cde3770756dafda2b017c299f87ed32e84c47f
Regina Bass <reginabassvdl@gmail.com>
2019-12-08 16:15:16 -0500

    Updated Menu Bar and working Manual ActionListener

--- CODE/Board.java
+++ CODE/Board.java
@@ -26,14 +26,16 @@ public class Board extends JFrame {
             JMenu jmFile = new JMenu("File");
             JMenuItem jmiExit = new JMenuItem("Exit");
             
-            JMenu jmAbout = new JMenu("About");
+            JMenu jmManual = new JMenu("Manual");
             JMenuItem jmKey = new JMenuItem("Key");
             JMenuItem jmiRules = new JMenuItem("Rules");
             JMenuItem jmiDisclaimer = new JMenuItem("Disclaimer");
+            JMenuItem jmiAbout = new JMenuItem("About");
             
             jmFile.add(jmiExit);
-            jmAbout.add(jmiRules);
-            jmAbout.add(jmiDisclaimer);
+            jmManual.add(jmiRules);
+            jmManual.add(jmiDisclaimer);
+            jmManual.add(jmiAbout);
             
             //Action Listeners for Menu Items
             MyAdapter ma = new MyAdapter();
@@ -41,10 +43,11 @@ public class Board extends JFrame {
             jmiExit.addActionListener(ma);
             jmiRules.addActionListener(ma);
             jmiDisclaimer.addActionListener(ma);
+            jmiAbout.addActionListener(ma);
             jmiExit.addActionListener(ma);
             
             menu.add(jmFile);
-            menu.add(jmAbout);
+            menu.add(jmManual);
             menu.add(jmKey);
         this.setJMenuBar(menu);
          
@@ -134,7 +137,22 @@ public class Board extends JFrame {
             String choice = ae.getActionCommand();
             
             if( choice.equals("About") ){
-             System.out.println("About was clicked");
+               JFrame about = new JFrame("About");
+               about.setSize(400, 400);
+               about.setLocationRelativeTo(null);
+               StringBuilder htmlBuilder  = new StringBuilder();
+               htmlBuilder.append("<html>");
+               htmlBuilder.append("<h1>About</h1><br />");
+               htmlBuilder.append("<p>For our final group project in <i>Computational Problem-Solving in the information Domain II</i> ISTE-121, our group created a digital version of the popular kid's board game Candyland to which we added a small twist. ");
+               htmlBuilder.append("This program was written in java and assets were made entirely by the group members.");
+               htmlBuilder.append("To maximize interactiveness, we decided to let the users themselves have control over their pieces, and added a chat functionality on the side so that players can communicate with their opponents. ");
+               htmlBuilder.append("For your convenience, the rules/directions for this game were added to the Menu bar along with this about page, and there is a key/guide in the interface underneath the 'Draw' button that tells you what our special cards mean.</p>");
+               htmlBuilder.append("<br /><p>We hope you enjoy!</p><hr /><br /> <strong>Regina Bass</strong>  &nbsp; - &nbsp;2nd Year Web and Mobile Computing <br /> <strong>Amy Tan</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -  &nbsp;2nd Year Web and Mobile Computing <br /> <strong>Miki Mian</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   -  &nbsp;3rd Year Human Centered Computing</html>");
+               String html = htmlBuilder.toString();
+               JTextArea textareaAbout = new JTextArea(html);
+               textareaAbout.setEnabled(false);
+               about.add(textareaAbout);
+               about.setVisible(true); 
             }
             else if(choice.equals("Rules")){
                System.out.println("Rules was clicked");
